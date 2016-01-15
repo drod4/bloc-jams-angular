4514c02831a4c0a54833af22f21d07f1e52fcb4b
dylan <rodriguez.dylan4@yahoo.com>
2016-01-14 17:33:26 -0800

    commit checkpoint11 nw

--- dist/scripts/filters/timecode.js
+++ dist/scripts/filters/timecode.js
@@ -1,28 +1,28 @@
-(function () {
-    function timecode() {
-        return function (seconds) {
-            var seconds = Number.parseFloat(seconds);
-            
-            if (Number.isNaN(seconds)) {
-                return '-:--';
-            }
-            
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
+(function() {
+     function timecode() {
+         return function(seconds) 
+         var seconds = Number.parseFloat(seconds);
+         
+         if (Number.isNaN(seconds)) {
+         return '-:--';
+     }
+             var wholeSeconds = Math.floor(seconds);
+             var minutes = Math.floor(wholeSeconds / 60);
+             var remainingSeconds = wholeSeconds % 60;
  
-            var output = minutes + ':';
+             var output = minutes + ':';
  
-            if (remainingSeconds < 10) {
-                output += '0';   
-            }
+             if (remainingSeconds < 10) {
+                 output += '0';   
+             }
  
-            output += remainingSeconds;
-            return output;
-        };
-    }
-
-    angular
-        .module('blocJams')
-        .filter('timecode', timecode);
-})();
+             output += remainingSeconds;
+ 
+             return output;
+         };
+     }
+ 
+     angular
+         .module('blocJams')
+         .filter('timecode', timecode);
+ })();
